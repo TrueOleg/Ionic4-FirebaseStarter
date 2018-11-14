37a1d2300acdec6250abdee42098c23c3e50f38b
trueoleg <trueolegzaichkin@gmail.com>
2018-11-14 08:18:51 +0300

    add log out

--- src/app/pages/account/account.page.ts
+++ src/app/pages/account/account.page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from '../../services/auth-service/auth.service';
+
 @Component({
   selector: 'app-account',
   templateUrl: 'account.page.html',
   styleUrls: ['account.page.scss']
 })
-export class AccountPage { }
+export class AccountPage {
+
+  constructor(private auth: AuthService) { }
+
+  logOut() {
+    this.auth.logOut();
+  }
+}
--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -1,15 +1,43 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+// import { AngularFireDatabase } from '@angular/fire/database';
 import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+// import { switchMap } from 'rxjs/operators';
+// import * as firebase from 'firebase';
+// import 'rxjs/add/operator/switchMap';
+
+export class User {
+
+  uid: string;
+  username = '';
+
+  constructor(auth) {
+    this.uid = auth.uid;
+  }
+
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   authenticationState = new BehaviorSubject(false);
+  currentUser: User;
 
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(public afAuth: AngularFireAuth) {
+    // this.afAuth.authState.switchMap(auth => {
+    //   if (auth) {
+    //     this.currentUser = new User(auth);
+    //     return this.db.object(`/users/${auth.uid}`);
+    //   } else { return []; }
+    // })
+    //   .subscribe(user => {
+    //     this.currentUser['username'] = user.username;
+    //   });
+  }
+
+
 
   signInWithEmail(login) {
     console.log('Sign in with email');
@@ -27,6 +55,10 @@ export class AuthService {
       .then(() => {
         this.authenticationState.next(true);
       });
+  }
+
+  logOut() {
+    this.authenticationState.next(false);
   }
 
   isAuthenticated() {
